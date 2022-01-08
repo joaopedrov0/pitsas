96ba848c750409c1e01a5710ad00b58eb69c52ce
João Pedro V <mastergamerjp06@gmail.com>
2022-01-08 03:11:08 +0000

    bug do modal corrigido

--- public/javascripts/pedido.js
+++ public/javascripts/pedido.js
@@ -1,11 +1,23 @@
 document.querySelector('.blocker').addEventListener('click', toggleModal)
 
 document.querySelector('#fazer-pedido button').addEventListener('click', toggleModal)
-
+var evento
 function toggleModal(e) {
-    console.log(e)
-    if(!e.target.classList.contains('blocker') & e.path[0] != 'button' ) return
+    if(!e.target.classList.contains('allowed')) return
 
     document.querySelector('.blocker').classList.toggle('closed')
+    
 }
 
+var pedidosInput = document.querySelector('textarea.pedidos')
+
+var pedidos = []
+
+function adicionar(sabor){
+    pedidos.push(sabor)
+    renderInput()
+}
+
+function renderInput(){
+    pedidosInput.value = pedidos.join(', ')
+}
